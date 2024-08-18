076464db2fd581b62312695e29a584d89dd61318
Godfredasare <godasare8346@gmail.com>
2024-08-18 19:02:04 +0000

    added deleteFromCloudinary in utils

--- utils/cloudinary.go
+++ utils/cloudinary.go
@@ -39,3 +39,19 @@ func UploadToCloudinary(file *multipart.FileHeader) (string, string, error) {
 	return resp.PublicID, resp.SecureURL, nil
 
 }
+
+func DeleteFromCloudinary(imageId string) error {
+	var cloudinary_url = os.Getenv("CLOUDINARY_URL")
+
+	cld, err := cloudinary.NewFromURL(cloudinary_url)
+	if err != nil {
+		return errors.New("error with cloudinary url")
+	}
+
+	resp, err := cld.Upload.Destroy(context.Background(), uploader.DestroyParams{PublicID: imageId})
+	if err != nil || resp.Result != "ok" {
+		return errors.New("error deleting deleting images from cloudinary")
+	}
+
+	return nil
+}
